90f97b1d408175a37c9c51495d81a115793dc545
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2021-12-03 15:38:55 +0100

    programmation de previous

---
+++ src/dijkstra/Previous.java
@@ -0,0 +1,21 @@
+package dijkstra;
+
+import java.util.Hashtable;
+
+public class Previous implements PreviousInterface {
+
+	private final Hashtable<VertexInterface, VertexInterface> hashtable = new Hashtable<VertexInterface, VertexInterface>() ;
+	
+	public void setPrevious(VertexInterface x, VertexInterface previous) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public VertexInterface getPrevious(VertexInterface x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
