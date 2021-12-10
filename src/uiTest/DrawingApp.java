cde2eaa6701790ccb8f26faf199e3746db2d9039
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2021-12-10 11:10:30 +0100

    implementation de l'ihm test

---
+++ src/uiTest/DrawingApp.java
@@ -0,0 +1,25 @@
+package uiTest;
+
+import javax.swing.*;
+
+public class DrawingApp extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final DrawingMenuBar drawingMenuBar;
+	private final WindowPanel windowPanel;
+
+	public DrawingApp() {
+		super("Drawing Application"); // Window title
+
+		setJMenuBar(drawingMenuBar = new DrawingMenuBar(this));
+
+		setContentPane(windowPanel = new WindowPanel(this)) ;
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Explicit !
+		pack(); // Components sizes and positions
+		setVisible(true); // The great show
+	}
+
+}
