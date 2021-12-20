7431c7f6d82d751dbb4c1d3439de66081d8b6e7a
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2021-12-20 19:35:40 +0100

    ajout d'une classe PBox qui sera écessaire pour la construction de l'interface

---
+++ src/algo/maze/PBox.java
@@ -0,0 +1,15 @@
+package algo.maze;
+
+/**
+ * Classe des cases du chemin final, sera utile pour l'interface graphique
+ * 
+ * @author Tristan Perrot
+ *
+ */
+public class PBox extends MBox {
+
+	protected PBox(int i, int j) {
+		super(i, j, ".");
+	}
+
+}
