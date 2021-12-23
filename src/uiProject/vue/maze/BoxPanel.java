d95cf654cd41daed3d942d2e876dae0edf15eef5
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2021-12-23 22:09:35 +0100

    ajout des images

--- src/uiProject/vue/maze/BoxPanel.java
+++ src/uiProject/vue/maze/BoxPanel.java
@@ -1,5 +1,11 @@
 package uiProject.vue.maze;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import algo.maze.MBox;
@@ -15,6 +21,24 @@ public class BoxPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private final MBox box;
 	private BoxPanelMouseListener mazePanelMouseListener;
+	
+	private static Image arrival ;
+	private static Image wall ;
+	private static Image departure ;
+	private static Image empty ;
+	private static Image path;
+	
+	static { 
+		try {
+			arrival = ImageIO.read(new File("data/arrival.jpg"));
+			wall =ImageIO.read(new File("data/wall.jpg"));
+			empty = ImageIO.read(new File("data/empty.jpg"));
+			departure = ImageIO.read(new File("data/departure.png"));
+			path = ImageIO.read(new File("data/path.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public BoxPanel(MazeApp mazeApp, MBox box) {
 		this.box = box;
@@ -28,6 +52,24 @@ public class BoxPanel extends JPanel {
 	public MBox getBox() {
 		return box;
 	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+	   // Paint the background
+	   super.paintComponent(g) ;
+
+	   if(box.getLabel() =="A") {
+		    g.drawImage(arrival, 0,0,getWidth(),getHeight(),this);
+	    }else if( box.getLabel() == "D") {
+		    g.drawImage(departure, 0,0,getWidth(),getHeight(),this);
+		}else if (box.getLabel() == "E") {
+			g.drawImage(empty, 0,0,getWidth(),getHeight(),this);
+		}else if (box.getLabel() == "W") {
+			g.drawImage(wall, 0,0,getWidth(),getHeight(),this);
+		}else {
+			g.drawImage(path, 0,0,getWidth(),getHeight(),this);
+		}
+	} 
 
 	/**
 	 * Action to do if there is an update of the box
