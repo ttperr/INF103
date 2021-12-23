fcbac9a0320d386f08c841655ffdf020e68e1044
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2021-12-23 23:22:01 +0100

    ajout du main

---
+++ src/projectLauncher/MainUi.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package projectLauncher;
+
+import uiProject.vue.MazeApp;
+
+/**
+ * @author Tristan Perrot
+ *
+ */
+public class MainUi {
+
+	public static void main(String[] args) {
+		new MazeApp();
+	}
+}
