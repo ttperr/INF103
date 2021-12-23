2e1d410f89080c3a5a3c11fdde46a5a2cee74aff
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2021-12-23 22:25:22 +0100

    ajout des cases de sélections

--- src/uiProject/vue/selectBox/SelectBoxPanel.java
+++ src/uiProject/vue/selectBox/SelectBoxPanel.java
@@ -1,5 +1,23 @@
 package uiProject.vue.selectBox;
 
-public class SelectBoxPanel {
+import java.awt.GridLayout;
 
+import javax.swing.JPanel;
+
+import uiProject.vue.MazeApp;
+
+public class SelectBoxPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private final TypeBoxButton eTypeButton;
+	private final TypeBoxButton wTypeButton;
+	private final TypeBoxButton aTypeButton;
+	private final TypeBoxButton dTypeButton;
+
+	public SelectBoxPanel(MazeApp mazeApp) {
+		setLayout(new GridLayout(4, 1));
+		add(eTypeButton = new TypeBoxButton(mazeApp, "Empty"));
+		add(wTypeButton = new TypeBoxButton(mazeApp, "Wall"));
+		add(aTypeButton = new TypeBoxButton(mazeApp, "Arrival"));
+		add(dTypeButton = new TypeBoxButton(mazeApp, "Departure"));
+	}
 }
--- src/uiProject/vue/selectBox/TypeBoxButton.java
+++ src/uiProject/vue/selectBox/TypeBoxButton.java
@@ -1,5 +1,26 @@
 package uiProject.vue.selectBox;
 
-public class TypeBoxButton {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+
+import uiProject.model.MazeAppModel;
+import uiProject.vue.MazeApp;
+
+public class TypeBoxButton extends JButton implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	private final MazeApp mazeApp;
+
+	public TypeBoxButton(MazeApp mazeApp, String label) {
+		super(label);
+		this.mazeApp = mazeApp;
+		addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent evt) {
+		MazeAppModel mazeAppModel = mazeApp.getMazeAppModel();
+		String label = this.getText();
+		mazeAppModel.setSelectedBoxLabel(label);
+	}
 }
