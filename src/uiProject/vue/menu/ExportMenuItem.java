f063d66199d8bfd014b666ae5126e35eaf918f10
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2022-01-03 23:34:34 +0100

    ajout du file chooser pour l'export

--- src/uiProject/vue/menu/ExportMenuItem.java
+++ src/uiProject/vue/menu/ExportMenuItem.java
@@ -3,6 +3,7 @@ package uiProject.vue.menu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
@@ -30,12 +31,11 @@ public class ExportMenuItem extends JMenuItem implements ActionListener {
 	 */
 	public void actionPerformed(ActionEvent e) {
 		MazeAppModel mazeAppModel = mazeApp.getMazeAppModel();
-		String fileName = JOptionPane.showInputDialog(mazeApp, "Text file name :");
-		while (fileName != null && fileName.length() == 0) {
-			fileName = JOptionPane.showInputDialog(mazeApp, "File name cannot be empty ! Text file name :", "export");
-		}
-		if (fileName != null) {
-			mazeAppModel.exportMazeToTextFile(fileName);
+		JFileChooser jFileChooser = new JFileChooser("data/");
+		jFileChooser.addChoosableFileFilter(new FileNameExtensionFilter("*.txt", "txt"));
+		int response = jFileChooser.showSaveDialog(mazeApp, "Export your maze");
+		if(response == JFileChooser.APPROVE_OPTION) {
+			mazeAppModel.exportMazeToTextFile();
 			JOptionPane.showMessageDialog(mazeApp, "File exported !");
 			mazeAppModel.setExported(true);
 		}
--- src/uiProject/vue/menu/QuitMenuItem.java
+++ src/uiProject/vue/menu/QuitMenuItem.java
@@ -3,6 +3,7 @@ package uiProject.vue.menu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
@@ -33,20 +34,20 @@ public class QuitMenuItem extends JMenuItem implements ActionListener {
 		MazeAppModel mazeAppModel = mazeApp.getMazeAppModel();
 
 		if (!mazeAppModel.isExported()) {
-			int response = JOptionPane.showInternalOptionDialog(this, "Maze not saved. Export it ?", "Quit application",
+			int response = JOptionPane.showInternalOptionDialog(mazeApp, "Maze not saved. Export it ?", "Quit application",
 					JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, null, null, null);
 			switch (response) {
 			case JOptionPane.CANCEL_OPTION:
 				return;
 			case JOptionPane.OK_OPTION:
-				String fileName = JOptionPane.showInputDialog(mazeApp, "Text file name :");
-				while (fileName.equals("") || fileName.equals(null)) {
-					fileName = JOptionPane.showInputDialog(mazeApp, "File name cannot be empty ! Text file name :",
-							"export");
+				JFileChooser jFileChooser = new JFileChooser("data/");
+				jFileChooser.addChoosableFileFilter(new FileNameExtensionFilter("*.txt", "txt"));
+				int response = jFileChooser.showSaveDialog(mazeApp, "Export your maze");
+				if(response == JFileChooser.APPROVE_OPTION) {
+					mazeAppModel.exportMazeToTextFile();
+					JOptionPane.showMessageDialog(mazeApp, "File exported !");
+					mazeAppModel.setExported(true);
 				}
-				mazeAppModel.exportMazeToTextFile(fileName);
-				JOptionPane.showMessageDialog(mazeApp, "File exported !");
-				mazeAppModel.setExported(true);
 				break;
 			case JOptionPane.NO_OPTION:
 				break;
